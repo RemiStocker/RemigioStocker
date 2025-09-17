27f030275be90fe6f950db6d93ee48538de9dddb
RemiStocker <remigio.stocker@sellside.cl>
2025-09-17 03:09:56 +0000

    feat: Agregar módulo base simple_api_custom

---
+++ simple_api_custom/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Boletas de Honorarios SimpleAPI',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting/Localization',
+    'summary': 'Integración con SimpleAPI para emisión automática de boletas de honorarios',
+    'description': """
+        Módulo para la creación y autodescarga de boletas de honorarios
+        mediante integración con SimpleAPI Chile.
+        
+        Características:
+        - Emisión automática de boletas de honorarios
+        - Descarga automática de PDF
+        - Integración directa con SII Chile
+        - Gestión de estados y seguimiento
+    """,
+    'author': 'Tu Empresa',
+    'website': 'https://www.tuempresa.com',
+    'license': 'LGPL-3',
+    'depends': ['base', 'account', 'contacts', 'mail','web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/boleta_honorarios_views.xml',
+        'views/res_config_settings_views.xml',
+        #'data/ir_cron_data.xml',
+        
+    ],
+    'assets': {
+        'web.assets_backend': [
+            #'boleta_honorarios_simpleapi/static/src/js/preview_iframe.js',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
